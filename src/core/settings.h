f0ebcb15ab1448728c030d92dd038df1967791fb
eqdiag <eqdiag@gmail.com>
2024-01-23 20:45:03 -0500

    fixed auto-gen file issue

--- src/core/settings.h
+++
@@ -1,4 +0,0 @@
-#define ASSET_DIR  "/Users/eqdiag/code/lightBx/assets/" 
-#define SHADER_DIR  "/Users/eqdiag/code/lightBx/assets/shaders/"  
-#define MODEL_DIR  "/Users/eqdiag/code/lightBx/assets/models/" 
-#define IMAGE_DIR  "/Users/eqdiag/code/lightBx/assets/textures/" 
